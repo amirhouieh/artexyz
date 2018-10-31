1829a83fa0018ccba5c9482105cc293418c4d532
amir houieh <xtxt@amirs-iMac.local>
2018-10-31 17:04:34 +0100

    fix seo tags

--- src/components/seo.tsx
+++ src/components/seo.tsx
@@ -30,16 +30,12 @@ export const Seo: React.SFC<IProps> = ({seo, pathname = "", sitedata}) => {
     return (
         <Head>
 
-            <meta name={`keywords`} content={sitedata.tags}/>
-
             <link rel="icon"
                   type="image/png"
                   href={`${baseUrl}/images/${sitedata.thumb.filename}`}/>
 
-            <meta
-                name={`description`}
-                content={`${description}`}
-            />
+            <meta name={`keywords`} content={sitedata.tags}/>
+            <meta name={`description`} content={`${description}`} />
 
             <meta name={`copyright`} content={`amir houieh`}/>
             <meta name={`language`} content={`EN`}/>
@@ -49,17 +45,16 @@ export const Seo: React.SFC<IProps> = ({seo, pathname = "", sitedata}) => {
             <meta name={`owner`} content={`amir houieh`}/>
             <meta name={`url`} content={url}/>
             <meta name={`identifier-URL`} content={url}/>
-            <meta name={`og:title`} content={title}/>
-            <meta name={`og:url`} content={url}/>
-            <meta name={`og:image`} content={imgUrl}/>
-            <meta name={`og:image:type`} content={`image/jpg`}/>
-            <meta name={`og:image:width`} content={`${image.width}`}/>
-            <meta name={`og:image:height`} content={`${image.height}`}/>
-            <meta name={`og:site_name`} content={title}/>
-            <meta
-                name={`og:description`}
-                content={`${description}`}
-            />
+
+            <meta property={`og:title`} content={title}/>
+            <meta property={`og:url`} content={url}/>
+            <meta property={`og:image`} content={imgUrl}/>
+            <meta property={`og:image:type`} content={`image/jpg`}/>
+            <meta property={`og:image:width`} content={`${image.width}`}/>
+            <meta property={`og:image:height`} content={`${image.height}`}/>
+            <meta property={`og:site_name`} content={title}/>
+            <meta property={`og:description`} content={`${description}`} />
+            <meta property={`og:type`} content={"website"} />
 
             <title>{title}</title>
 
